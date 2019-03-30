518f69aa966e2bb0c4ef8cda62db11133da11ded
itshoom <hoomstory@naver.com>
2019-03-30 19:10:18 +0900

    third

--- password-retry.py
+++ password-retry.py
@@ -1,13 +1,14 @@
 password = '123456'
 x = 3
 while x > 0:
+    x -= 1
     pwd = input('请输入密码: ')
     if pwd == password:
         print('登陆成功!')
         break
     else:
-        x -= 1
-        if x >= 1:
-            print('密码错误,还剩', x, '次机会')
-        if x == 0:
+        print('密码错误,')
+        if x > 0:
+            print('还剩', x, '次机会')
+        else:
             print('请重置密码')
